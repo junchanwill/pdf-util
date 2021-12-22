fa50bac7ce37a7e3c1a45a97993f8b44b51add65
Junichi Nishimura <junichi.nishimura@will.company>
2021-12-22 11:21:26 +0900

    PDF結合処理を実装

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,27 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import org.apache.pdfbox.io.MemoryUsageSetting
+import org.apache.pdfbox.multipdf.PDFMergerUtility
+import org.apache.pdfbox.pdmodel.PDDocument
+import java.io.File
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    // ファイルが指定されてなかったら終了
+    if (args.isEmpty()) return
+
+    // 出力ファイルの頭に日付をつけるための処理
+    val ldt = LocalDateTime.now()
+    val format = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss")
+
+    try {
+        val pdDocument = PDDocument()
+        val merger = PDFMergerUtility()
+        args.forEach {
+            merger.addSource(File(it))
+        }
+        merger.destinationFileName = ldt.format(format) + "merged.pdf"
+        merger.mergeDocuments(MemoryUsageSetting.setupMainMemoryOnly())
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
 }
